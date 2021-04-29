37b46f27a4656627eff58b8ac3c50cad25c84c26
PresPetrov <preslav1000@gmail.com>
2021-04-29 19:25:49 +0300

    Added test case for Put

--- src/test/java/myHashMap/PutTest.java
+++ src/test/java/myHashMap/PutTest.java
@@ -2,6 +2,12 @@ package myHashMap;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.junit.Assert.*;
 public class PutTest {
 
@@ -26,5 +32,30 @@ public class PutTest {
         map.put("Fish", "Nemo");
         map.put("Fish", "Dory");
         assertEquals(1, map.size());
+    }
+    
+    @Test
+    public void putManyObjectsShouldSizeProperly() {
+        MyHashMap<String, String> map = new MyHashMap<>();
+        int size = 500000;
+        int keyLength = 20;
+        ThreadLocalRandom randGenerator = ThreadLocalRandom.current();
+        Set<String> keySet = new HashSet<>(650);
+        StringBuilder builder = new StringBuilder(10);
+        while(size > keySet.size()){
+            for(int i = 0; i < keyLength; i++) {
+                int rand = randGenerator.nextInt(52);
+                char base = (rand < 26) ? 'A' : 'a';
+                char character = (char) (base + rand % 26);
+                builder.append(character);
+            }
+            keySet.add(builder.toString());
+            builder.setLength(0);
+        }
+        List<String> keyList = new ArrayList<>(keySet);
+        for(String key : keyList){
+            map.put(key, "TestValue");
+        }
+        assertEquals(size, map.size());
     }
 }
